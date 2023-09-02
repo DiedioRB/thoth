b332f990324a9dcb62770c3dc202a43226897715
AHRGO <victor.030farias@gmail.com>
2023-09-02 08:18:24 -0300

    Merge do apocalipse, pt.2, corrigindo erros locais

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,8 @@ import 'package:thoth/views/tela_login.dart';
 import 'package:thoth/views/tela_menu.dart';
 import 'package:thoth/views/tela_quizzes.dart';
 import 'package:thoth/views/tela_teste_pergunta.dart';
+import 'package:thoth/views/tela_cadastro_quiz.dart';
+import 'package:thoth/views/tela_cadastro_perguntas.dart';
 
 class Routes {
   static const String home = "/";
@@ -12,7 +14,10 @@ class Routes {
   static const String cadastro = "/cadastro";
   static const String quizzes = "/quizzes";
   static const String quizzesForm = "/quizzes/form";
+  static const String cadastroQuiz = "/quizzes/cadastro";
   static const String perguntasTeste = "/perguntas/testes";
+  static const String cadastroPerguntas = "/perguntas/cadastro";
+  //static const String editarQuiz = "/quizzes/editar"; - Comentado para verificar a necessidade
 
   static var routes = <String, WidgetBuilder>{
     home: (context) => Login(),
@@ -20,6 +25,8 @@ class Routes {
     cadastro: (context) => Cadastro(),
     menu: (context) => const Menu(),
     quizzes: (context) => Quizzes(),
+    cadastroQuiz: (context) => CadastroQuiz(),
     perguntasTeste: (context) => PerguntaTeste(),
+    cadastroPerguntas: (context) =>CadastroPergunta()
   };
 }

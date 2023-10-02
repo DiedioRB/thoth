c41a369a8a179edf104be7b9a8907ffdf85f4f52
AHRGO <victor.030farias@gmail.com>
2023-10-02 07:17:33 -0300

    WIP

--- lib/views/atividade_flashcard.dart
+++ lib/views/atividade_flashcard.dart
@@ -4,6 +4,7 @@ import 'package:thoth/models/pergunta.dart';
 import 'package:flip_card/flip_card.dart';
 import 'package:thoth/tema_app.dart';
 import 'dart:math';
+import 'dart:async';
 
 class AtividadeFlashcard extends StatefulWidget {
   final Deck deck;
@@ -24,7 +25,12 @@ class _AtividadeFlashcardState extends State<AtividadeFlashcard> {
   bool _isCardFlipped = false;
   GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
   String resposta = "";
+  //String respostaPontos = "";
   String textoBotao = "Próximo";
+  late Timer _timer;
+  int _segundos = 0;
+  List<String> pontosCard = [];
+  bool _verificar = true;
 
 
 
@@ -33,12 +39,11 @@ class _AtividadeFlashcardState extends State<AtividadeFlashcard> {
     super.initState();
     _deck = widget.deck;
     _getPerguntas();
-
+    _startTimer();
   }
 
   void _getPerguntas() async {
     _perguntas = await _deck!.perguntas;
-
     if(qtdPerguntas < _perguntas.length) {
       while(perguntasQuiz.length < qtdPerguntas) {
         int indiceAleatorio = aleatorio.nextInt(_perguntas.length);
@@ -52,9 +57,31 @@ class _AtividadeFlashcardState extends State<AtividadeFlashcard> {
     }
 
     setState(() {});
-
   }
 
+
+  void _startTimer() {
+    const umSegundo = const Duration(seconds: 1);
+    _timer = Timer.periodic(umSegundo, (Timer timer) {
+      setState(() {
+        _segundos++;
+      });
+    });
+  }
+
+  void _calculaPontos(int secs) {
+    if (secs < 5) {
+      pontosCard.add("Muito Bom!");
+    } else if (secs < 10) {
+      pontosCard.add("Bom, mas ainda pode melhorar :D");
+    } else {
+      pontosCard.add("É, essa não foi legal. Continue treinando! :D");
+    }
+
+    print("segundos: $secs");
+
+
+  }
 
 
 
@@ -71,8 +98,14 @@ class _AtividadeFlashcardState extends State<AtividadeFlashcard> {
       Center(
         child: Column(
           children: [
+            Text("TEMPO: $_segundos",
+              style: TextStyle(
+                fontSize: 20,
+                color: TemaApp.contrastSecondary
+              ),
+            ),
             Container(
-                margin: const EdgeInsets.only(top: 100),
+                margin: const EdgeInsets.only(top: 50),
                 padding: const EdgeInsets.all(25),
                 width: 380,
                 height: 380,
@@ -84,6 +117,9 @@ class _AtividadeFlashcardState extends State<AtividadeFlashcard> {
                   key: cardKey,
                   onFlip: () {
                     setState(() {
+                      if(!_isCardFlipped) {
+                        _calculaPontos(_segundos);
+                      }
                       _isCardFlipped = true;
                       resposta = perguntasQuiz[count].resposta;
                     });
@@ -117,47 +153,91 @@ class _AtividadeFlashcardState extends State<AtividadeFlashcard> {
                   ),
                 ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 50),
-              height: 85,
-              width: 225,
-              decoration: BoxDecoration(
-                color: TemaApp.darkPrimary
-              ),
-              child: InkWell(
-                onTap: () {
-                  setState(() {
-
-                    if (_isCardFlipped) {
-                      cardKey.currentState!.toggleCard();
-                      resposta = "";
-                      _isCardFlipped = false;
-                    }
-
-                    if(count + 1 < perguntasQuiz.length) {
-                      if (count + 2 == perguntasQuiz.length) {
-                        textoBotao = "Finalizar";
-                      }
-                      count++;
-                    } else {
-                      count = count;
-                    }
-
-                  });
-                },
-                child: Center(
-                  child: Text(textoBotao,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
+            _verificar?
+              Container(
+                margin: const EdgeInsets.only(top: 50),
+                height: 85,
+                width: 150,
+                decoration: BoxDecoration(
+                    color: TemaApp.darkPrimary
+                ),
+                child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        _timer.cancel();
+                        _segundos = 0;
+                        _verificar = false;
+                      });
+                    },
+                    child: Center(
+                      child: Text("Verificar",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    )
                 )
-              )
+            ):
+            Container(
+              height: double.maxFinite,
+              child: Column(
+                children: [
+                  Text("pontosCard[count]"),
+                  Container(
+                      margin: const EdgeInsets.only(top: 50),
+                      height: 85,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          color: TemaApp.darkPrimary
+                      ),
+                      child: InkWell(
+                          onTap: () {
+                            setState(() {
+
+                              if (_isCardFlipped) {
+                                cardKey.currentState!.toggleCard();
+                                resposta = "";
+                                _isCardFlipped = false;
+                              }
+
+                              if(count + 1 < perguntasQuiz.length) {
+                                if (count + 2 == perguntasQuiz.length) {
+                                  textoBotao = "Finalizar";
+                                }
+                                count++;
+                              } else {
+                                count = count;
+                              }
+
+                              _startTimer();
+
+                            });
+                          },
+                          child: Center(
+                            child: Text(textoBotao,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          )
+                      )
+                  )
+                ],
+              ),
             )
+
+
           ],
         ),
       )
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
   }
 }

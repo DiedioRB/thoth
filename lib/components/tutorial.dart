fd4f436ffab0d619e1b0efed39ba1aea16d9f85e
Victor Hugo <victor.030farias@gmail.com>
2023-10-28 03:37:20 -0300

    Finalizado textos de tutorial

--- lib/components/tutorial.dart
+++ lib/components/tutorial.dart
@@ -28,7 +28,9 @@ class Tutorial {
         ),
         const Divider(),
         Text(
-            "O quizz é uma atividade recreativa que inspira a aprender através de perguntas e respostas (...)",
+            "O quiz é um tipo de teste que tem como objetivo avaliar o conhecimento de uma pessoa sobre um determinado assunto. \n"
+                "Ele consiste de uma série de perguntas com múltipla escolha, "
+                "nas quais o participante responde para ver quão bem ele ou ela entende sobre o tópico abordado.",
             style: TextStyle(
               color: TemaApp.quizSecondary,
             )
@@ -45,8 +47,10 @@ class Tutorial {
         ),
         const Divider(),
         Text(
-          "Para realizar o quizz, primeiro selecione um dos quizzes a seguir, orientados dentro do seu tópico de estudo.\n"
-              "Em seguida, aparecerá uma pergunta na tela, selecione a resposta correta (...)",
+          "Para realizar a atividade, selecione um dos quizzes na próxima tela.\n"
+              "Em seguida, você verá uma pergunta e algumas opções de resposta."
+              "Selecione a resposta que você julgar correta e toque em \"próximo\". \n"
+              "Ao fim da atividade, uma tela com a sua pontuação será apresentada",
             style: TextStyle(
               color: TemaApp.quizSecondary,
             )
@@ -82,7 +86,9 @@ class Tutorial {
         ),
         const Divider(),
         Text(
-            "O flashcard é uma atividade de memorização (...)",
+            "O flashcard é uma atividade de aprendizado que consiste em um cartão dividido em duas partes: "
+                "uma contém uma pergunta, palavra-chave ou conceito, e a outra contém a resposta ou explicação correspondente. "
+                "Flashcards são usados como uma técnica de memorização para revisar e reforçar o aprendizado.",
           style: TextStyle(
               color: TemaApp.flashcardSecondary,
           ),
@@ -99,7 +105,9 @@ class Tutorial {
         ),
         const Divider(),
         Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque pulvinar ligula a tincidunt. Class. (...)",
+            "Você verá um cartão com uma pergunta. Responda à pergunta mentalmente e toque no cartão para revelar a resposta. "
+                "Em seguida, escolha uma das opções que melhor descrevem a sua dificuldade em lembrar da resposta. \n"
+                "Esse é um exercício autoavaliativo, logo a falta de honestidade com suas respostas acarretará em prejuízo somente para seu aprendizado.",
             style: TextStyle(
               color: TemaApp.flashcardSecondary,
             )
@@ -135,7 +143,10 @@ class Tutorial {
         ),
         const Divider(),
         Text(
-            "O quizz é uma atividade recreativa que inspira a aprender através de perguntas e respostas (...)",
+            "O kart é uma maneira inovadora de gamificar o aprendizado. "
+                "Nele o participante controlará um carro e ao coletar as moedas, "
+                "uma pergunta, justo de suas respostas, será apresentada na tela. \n"
+                "Dessa forma, a atividade o Kart visa amenizar a dificuldade de aprendizado e torná-la mais instigante.",
             style: TextStyle(
               color: TemaApp.kartSecondary,
             )
@@ -152,7 +163,10 @@ class Tutorial {
         ),
         const Divider(),
         Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque pulvinar ligula a tincidunt. Class. (...)",
+            "Você controlará um carro. Toque na direção que deseja mover o carro e colete as moedas que aparecerem na pista. \n"
+                "A cada moeda coletada, uma nova parte da pergunta aparecerá no topo da tela. "
+                "Coletadas todas as moedas. Serão apresentadas as respostas para aquela pergunta. Selecione a resposta que julgar correta "
+                "e continue o jogo. Ao fim da atividade, será apresentada a sua pontuação",
             style: TextStyle(
               color: TemaApp.kartSecondary,
             )
